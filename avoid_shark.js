f9fab959d72ffb772ef73b5509291be926b70803
Tigrcub <48540725+Tigrcub@users.noreply.github.com>
2025-11-18 01:26:56 -0700

    Create avoid_shark.js

---
+++ avoid_shark.js
@@ -0,0 +1,9 @@
+// =========== AVOID_THE_SHARK Placeholder ========
+function playAvoidTheShark() {
+  // Placeholder for Oscar's game
+  background(0, 40, 80);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  text("Avoid The Shark Placeholder", width / 2, height / 2);
+}
